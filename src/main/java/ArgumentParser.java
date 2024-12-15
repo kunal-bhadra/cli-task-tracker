8756b30d4a07b4e6f62e5b3f1d11e2032dfc48b2
kunal.bhadra <rick101001@gmail.com>
2024-12-15 11:35:14 +0530

    feat(utils): parse JSON file with regex

--- src/main/java/ArgumentParser.java
+++ src/main/java/ArgumentParser.java
@@ -1,42 +1,14 @@
 package main.java;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ArgumentParser {
 
-    public int findMaxId(String filePath) {
-        int maxId = Integer.MIN_VALUE;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            Pattern idPattern = Pattern.compile("\"id\"\\s*:\\s*\"(\\d+)\"");
-
-            while ((line = reader.readLine()) != null) {
-                Matcher matcher = idPattern.matcher(line);
-                while (matcher.find()) {
-                    String idValueStr = matcher.group(1);
-                    try {
-                        int idValue = Integer.parseInt(idValueStr);
-                        maxId = Math.max(maxId, idValue);
-                    } catch (NumberFormatException e) {
-                        System.err.println("Invalid 'id' value: " + idValueStr);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return maxId;
-    }
+    ZoneId zone = ZoneId.of("Asia/Kolkata");
+    JsonParser jsonParser = new JsonParser();
 
     public boolean isInt(String str) {
         try {
@@ -47,36 +19,67 @@ public class ArgumentParser {
         }
     }
 
-    public String parseArguments(String[] args, boolean jsonExists, String jsonPath) {
+    public String addNewTask(String[] args, boolean jsonExists, String jsonPath) {
+
         // Find max ID if JSON file already exists
         int maxId = 0;
         if (jsonExists) {
-            maxId = findMaxId(jsonPath);
+            maxId = jsonParser.findMaxId(jsonPath);
+        }
+        final AtomicInteger count = new AtomicInteger(maxId);
+
+        // Add a new Task with required fields
+        Task task = new Task();
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Exactly 2 arguments are required!");
+        }
+        if (!isInt(args[1])) {
+            String description = args[1];
+            task.setDescription(description);
+            task.setId(count.incrementAndGet());
+            task.setCreatedAt(ZonedDateTime.now(zone).truncatedTo(ChronoUnit.SECONDS));
+            task.setUpdatedAt(ZonedDateTime.now(zone).truncatedTo(ChronoUnit.SECONDS));
+            task.setStatus(Task.Status.TODO);
+            System.out.println("Task added successfully (ID: " + task.getId() + ")");
         }
 
-        final AtomicInteger count = new AtomicInteger(maxId);
-        ZoneId zone = ZoneId.of("Asia/Kolkata");
+        // Return new JSON entry
+        return task.toJson();
+    }
 
-        // Make an object of Task
-        Task task = new Task();
+    public String listTasks(String[] args, boolean jsonExists, String jsonPath) {
 
-        // Add a task
+        // Check if JSON file exists
+        if(!jsonExists) {
+            throw new IllegalArgumentException("JSON File missing!");
+        }
+
+        // Print required tasks
+        if (args.length == 2) {
+            String requiredStatus = args[1];
+            if (!requiredStatus.matches("done|todo|in-progress")) {
+                throw new IllegalArgumentException("Only the following status is valid: done, todo, in-progress");
+            }
+            // list tasks with requiredStatus
+        }
+
+
+        return "NA";
+    }
+
+    public String parseArguments(String[] args, boolean jsonExists, String jsonPath) {
+
+        if (isInt(args[0])) {
+            throw new IllegalArgumentException("First argument should be a string!");
+        }
+
         String action = args[0];
         if (action.equals("add")) {
-            if (args.length < 2) {
-                throw new IllegalArgumentException("Exactly 2 arguments are required!");
-            }
-            if (!isInt(args[1])) {
-                String description = args[1];
-                task.setDescription(description);
-                task.setId(count.incrementAndGet());
-                task.setCreatedAt(ZonedDateTime.now(zone).truncatedTo(ChronoUnit.SECONDS));
-                task.setUpdatedAt(ZonedDateTime.now(zone).truncatedTo(ChronoUnit.SECONDS));
-                task.setStatus(Task.Status.TODO);
-                System.out.println("Task added successfully (ID: " + task.getId() + ")");
-            }
+            return addNewTask(args, jsonExists, jsonPath);
+        } else if (action.equals("list")) {
+            return listTasks(args, jsonExists, jsonPath);
+        } else {
+            throw new IllegalArgumentException("First argument is invalid!");
         }
-
-        return task.toJson();
     }
 }
---
+++ src/main/java/JsonParser.java
@@ -0,0 +1,55 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JsonParser {
+    public List<Task> parseJson(String filePath) {
+        List<Task> tasks = new ArrayList<>();
+        ZoneId zone = ZoneId.of("Asia/Kolkata");
+
+        // Regex pattern to match all fields
+        String regex = "\\{\"id\":\"(\\d+)\",\"description\":\"(.*?)\",\"status\":\"(.*?)\",\"createdAt\":\"(.*?)\",\"updatedAt\":\"(.*?)\"}";
+        Pattern pattern = Pattern.compile(regex);
+
+        // Add each task to list line by line
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    Task task = new Task();
+                    task.setId(Integer.parseInt(matcher.group(1)));
+                    task.setDescription(matcher.group(2));
+                    task.setStatus(Task.Status.valueOf(matcher.group(3)));
+                    task.setCreatedAt(LocalDateTime.parse(matcher.group(4)).atZone(zone));
+                    task.setUpdatedAt(LocalDateTime.parse(matcher.group(5)).atZone(zone));
+                    tasks.add(task);
+                }
+            }
+            return tasks;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int findMaxId(String filePath) {
+        int maxId = Integer.MIN_VALUE;
+        List<Task> tasks = parseJson(filePath);
+
+        // Extract max ID from list of Task objects
+        for (Task task: tasks) {
+            int taskId = task.getId();
+            maxId = Math.max(maxId, taskId);
+        }
+
+        return maxId;
+    }
+}
